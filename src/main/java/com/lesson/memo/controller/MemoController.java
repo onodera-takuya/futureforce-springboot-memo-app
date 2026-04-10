d377531058aa1ec2cd2f5913273af16de2120317
raion <raion@192.168.0.38>
2026-04-10 21:06:34 +0900

    ・新規登録画面のバリデーション時、優先度表示するよう修正
    優先度を表示するためControllerの/createメソッドにmodelコードの追加。
    
    一覧画面のソート機能を実装出来ていなかったためControllerにソートするコードを追加。合わせてimport文追加。
    
    優先度必須機能追加。
    優先度のバリデーションメッセージをjavaに追加。
    合わせてThymeleafをmemo-formに追加

--- src/main/java/com/lesson/memo/controller/MemoController.java
+++ src/main/java/com/lesson/memo/controller/MemoController.java
@@ -1,6 +1,7 @@
 package com.lesson.memo.controller;
 
 import java.time.LocalDateTime;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 
@@ -32,6 +33,7 @@ public class MemoController {
     @GetMapping
     public String list(Model model) {
         List<Memo> memos = memoRepository.findAll();
+        memos.sort(Comparator.comparing(Memo::getPriority));
         model.addAttribute("memos", memos);
         return "memo-list";
     }
@@ -45,8 +47,11 @@ public class MemoController {
 
     @PostMapping("/create")
     public String create(@ModelAttribute @Valid Memo memo,
-            BindingResult result) {
+            BindingResult result,
+            Model model
+    		) {
         if (result.hasErrors()) {
+        	model.addAttribute("priorities", Priority.values());
             return "memo-form";
         }
 
--- src/main/java/com/lesson/memo/model/Memo.java
+++ src/main/java/com/lesson/memo/model/Memo.java
@@ -4,12 +4,11 @@ import java.time.LocalDateTime;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
@@ -37,6 +36,6 @@ public class Memo {
     @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime updatedAt;
     
-    @Enumerated(EnumType.STRING)
+    @NotNull(message = "優先度を選択してください")
     private Priority priority;
 }
