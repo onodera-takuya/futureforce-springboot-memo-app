cb76d190d6289537d0958109ef44cdb1009b1895
raion <raion@192.168.0.38>
2026-04-11 19:33:52 +0900

    メモに全文検索機能の実装

--- src/main/java/com/lesson/memo/MemoApplication.java
+++ src/main/java/com/lesson/memo/MemoApplication.java
@@ -1,7 +1,10 @@
 package com.lesson.memo;
 
+
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 @SpringBootApplication
 public class MemoApplication {
--- src/main/java/com/lesson/memo/controller/MemoController.java
+++ src/main/java/com/lesson/memo/controller/MemoController.java
@@ -4,6 +4,9 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,13 +16,11 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.lesson.memo.model.Memo;
 import com.lesson.memo.repository.MemoRepository;
-
-import jakarta.servlet.http.HttpServletResponse;
-import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/memo")
@@ -39,6 +40,19 @@ public class MemoController {
     public String showForm(Model model) {
         model.addAttribute("memo", new Memo());
         return "memo-form";
+    }
+    
+    @GetMapping("/search")
+    public String search(@RequestParam(name = "keyword")String keyword, Model model) {
+    	model.addAttribute("keyword", keyword);
+    	List<Memo> memos;
+    	if (keyword == null || keyword.isBlank()) {
+    		memos = memoRepository.findAll();
+    	} else {
+    		memos = memoRepository.findByTitleContainingOrContentContaining(keyword, keyword);
+    	}
+    	model.addAttribute("memos", memos);
+    	return "memo-list";
     }
 
     @PostMapping("/create")
