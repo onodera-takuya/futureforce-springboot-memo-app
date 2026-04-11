2ca37490b35609ad6cf0d7107e5aec26504275e0
raion <raion@192.168.0.38>
2026-04-11 19:43:02 +0900

    不要な改行の削除

--- src/main/java/com/lesson/memo/MemoApplication.java
+++ src/main/java/com/lesson/memo/MemoApplication.java
@@ -1,10 +1,7 @@
 package com.lesson.memo;
 
-
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 
 @SpringBootApplication
 public class MemoApplication {
