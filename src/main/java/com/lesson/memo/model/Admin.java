e674278595ff54e646bf119845bd636055f2a3f8
raion <raion@192.168.0.38>
2026-05-15 10:28:47 +0900

    ログイン機能の実装(backend)

---
+++ src/main/java/com/lesson/memo/model/Admin.java
@@ -0,0 +1,51 @@
+package com.lesson.memo.model;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import lombok.Data;
+
+@Entity
+@Data
+public class Admin {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotBlank(message = "性を入力してください")
+    @Column(nullable = false)
+    @Size(max = 255)
+    private String last_name;
+
+    @NotBlank(message = "名を入力してください")
+    @Column(nullable = false)
+    @Size(max = 255)
+    private String first_name;
+    
+    @NotBlank(message = "メールアドレスを入力してください")
+    @Email(message = "正しいメールアドレス形式で入力してください")
+    @Column(nullable = false,unique = true)
+    @Size(max = 255)
+    private String email;
+    
+    @NotBlank(message = "パスワードを入力してください")
+    @Size(min = 8, message = "パスワードは8文字以上で入力してください")
+    private String password;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime createdAt;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime updatedAt;
+}
